35194cc073b8e07e1ba8cd4165d4b28185120c2e
Su Liang <suliangxd@gmail.com>
2016-03-16 14:03:33 +0800

    modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,20 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-login_manager.login_view = ''
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+
+
+
+
