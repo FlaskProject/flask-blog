0034629f3fec1b825ecb7825f8101dd1efa8b633
Su Liang <suliangxd@gmail.com>
2016-03-16 16:46:13 +0800

    modified forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,7 @@ from ..models import User
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField(Keep me logged in)
+    remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
 
 class RegistrationForm(Form):
