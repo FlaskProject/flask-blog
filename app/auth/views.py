31c6af3900a1b01984869de288186e510414092d
Su Liang <suliangxd@gmail.com>
2016-03-16 14:30:48 +0800

    modified app/auth/views.py

---
+++ app/auth/views.py
@@ -0,0 +1,23 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user, logout_user, login_required, \
+    current_user
+from . import auth
+from .. import db
+from ..models import  User
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm, ChangePasswordFrom,\
+    PasswordRestRequestForm, PasswordResetForm, ChangeEmailFrom
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+        and not current_user.confirmed \
+        and request.endpoint[:5] != 'auth.' \
+        and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
